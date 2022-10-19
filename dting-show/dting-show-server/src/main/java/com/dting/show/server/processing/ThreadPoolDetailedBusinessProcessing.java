2052824d14c532cb959980439dac6c5a110a0983
‘huangfu’ <huangfu_project@163.com>
2022-10-19 09:17:15 +0800

    删除多余的包

--- dting-show/dting-show-server/src/main/java/com/dting/show/server/processing/ThreadPoolDetailedBusinessProcessing.java
+++ dting-show/dting-show-server/src/main/java/com/dting/show/server/processing/ThreadPoolDetailedBusinessProcessing.java
@@ -2,7 +2,6 @@ package com.dting.show.server.processing;
 
 import com.alibaba.fastjson.JSON;
 import com.dting.message.common.handlers.DtingSimpleChannelInboundHandler;
-import com.dting.show.datas.TaskInfoMessage;
 import com.dting.show.datas.ThreadPoolDetailedMessage;
 import io.netty.channel.ChannelHandlerContext;
 
@@ -12,7 +11,7 @@ import io.netty.channel.ChannelHandlerContext;
  * @author huangfu
  * @date 2022年10月19日09:01:46
  */
-public class ThreadPoolDetailedBusinessProcessing  extends DtingSimpleChannelInboundHandler<ThreadPoolDetailedMessage> {
+public class ThreadPoolDetailedBusinessProcessing extends DtingSimpleChannelInboundHandler<ThreadPoolDetailedMessage> {
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, ThreadPoolDetailedMessage threadPoolDetailedMessage) throws Exception {
         System.out.println(JSON.toJSONString(threadPoolDetailedMessage));
