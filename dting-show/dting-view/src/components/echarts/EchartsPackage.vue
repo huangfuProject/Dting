9b84018d6ce9222eb2641c12e920cfff10c76fed
‘huangfu’ <huangfu_project@163.com>
2022-10-25 17:09:28 +0800

    前端界面封装 echarts组件

---
+++ dting-show/dting-view/src/components/echarts/EchartsPackage.vue
@@ -0,0 +1,60 @@
+<template>
+    <div :style="style"></div>
+</template>
+
+<script>
+import * as echarts from 'echarts';
+
+export default {
+    props: {
+        height: {
+            type:String,
+            default: "600px"
+        },
+        width: {
+            type:String,
+            default: "800px"
+        },
+        options: {}
+    },
+    data(){
+        return {
+            //echart对象
+            myChart:null
+        }
+    },
+    //挂载时调用
+    mounted(){
+        //HTMLElement
+        this.myChart = echarts.init(this.$el);
+        // 使用刚指定的配置项和数据显示图表。
+        this.myChart.setOption(this.options);
+    },
+    //计算属性  内部的计算值 可直接被外部绑定使用
+    computed:{
+        style(){
+            return {
+                    width: this.width,
+                    height: this.height
+            }
+        }
+    },
+    // 数据监听
+    watch:{
+        style(){
+            if(this.myChart){
+                this.myChart.resize({
+                    animation: {
+                        duration: 400
+                    }
+                });
+            }
+        },
+        options(){
+            if(this.myChart){
+                this.myChart.setOption(this.options)
+            }
+        }
+    }
+}
+</script>
