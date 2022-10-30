9433699ad38f6b65fb6ff4390d36ff27d25ade1e
huangfukexing <huangfu>
2022-10-30 21:15:27 +0800

    将折线图进行简单包装

---
+++ dting-show/dting-view/src/components/echarts/LineChart.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <!--Dting使用的折线图相关-->
+        <EchartsPackage :options="optionData"/>
+    </div>
+</template>
+
+<script>
+import EchartsPackage from './EchartsPackage.vue'
+export default {
+    name: "LineChart",
+    props:{
+        systemXAxisArray:[],
+        systemUse:[],
+        systemMax:[],
+        lineChartTitle:{
+            type: String,
+            default:""
+        },
+    },
+    computed: {
+        /**
+         * 资源用量
+         */
+        optionData(){
+            return {
+                title: {
+                text: this.lineChartTitle
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                data: ['总量','已使用']
+                },
+                xAxis: {
+                    type: 'category',
+                    show: true,
+                    boundaryGap: false,
+                    data: this.systemXAxisArray,
+                    axisLabel: {
+                        interval: 2
+                    }
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        name: '总量',
+                        type: 'line',
+                        step:true,
+                        data: this.systemMax,
+                        showSymbol: false
+                    },
+                    {
+                        name: '已使用',
+                        type: 'line',
+                        step:false,
+                        data: this.systemUse,
+                        showSymbol: false
+                    }
+                ]
+            }
+        }
+    },
+    components:{
+        EchartsPackage
+    },
+    watch: {
+        systemXAxisArray(){
+            //动态变更首尾数据
+            this.optionData.xAxis.axisLabel.interval=this.systemXAxisArray.length-2
+        }
+    }
+}
+</script>
--- dting-show/dting-view/src/views/monitors/SystemMonitorsOld.vue
+++ dting-show/dting-view/src/views/monitors/SystemMonitorsOld.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <!--系统内存监控折线图-->
-        <div><LineChart lineChartTitle="系统内存监控" :systemXAxisArray="systemXAxisArray" :systemUse="systemUseMemory" :systemMax="systemMaxMemory"/></div>
+        <div><EchartsPackage :options="systemMemoryOption"/></div>
     </div>
 </template>
 
 <script>
-import LineChart from '../../components/echarts/LineChart.vue'
+import EchartsPackage from '../../components/echarts/EchartsPackage.vue'
 import {initWebSocket, setCallback} from '../../utils/webSocket'
 import {post} from '../../utils/request'
 export default {
@@ -22,7 +22,7 @@ export default {
         }
     },
     methods:{
-        //可以将一个数据转换为图标需要的数据格式
+        //websocket返回的内存消息的处理器
         monitorsMemoryDataHandler(obj) {
             if (typeof obj == 'string') {
                 obj = JSON.parse(obj); 
@@ -66,23 +66,87 @@ export default {
                 this.connectWebsocket(monitorId);
             })
         },
-        // 将后端的内存数据解包为图标需要数据格式
+        // 设置系统内存数据
         refreshSystemMemoryData(res){
             const memoryDataList = res.memoryDataVo.systemMemoryDataList
             if(memoryDataList) {
                 for(var memoryData of memoryDataList) {
-                    //开始将内存数据映射到图表
+                    //调用消息回调
                     this.monitorsMemoryDataHandler(memoryData)
                 }
 
             }
         }
     },
+    computed: {
+        /**
+         * 系统内存监控数据
+         */
+        systemMemoryOption(){
+            return {
+                title: {
+                text: '系统内存监控'
+                },
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                data: ['系统总内存','系统已经使用的内存']
+                },
+                xAxis: {
+                    type: 'category',
+                    show: true,
+                    boundaryGap: false,
+                    data: this.systemXAxisArray,
+                    axisLabel: {
+                        interval: 2
+                    }
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        name: '系统总内存',
+                        type: 'line',
+                        step:true,
+                        data: this.systemMaxMemory,
+                        showSymbol: false
+                    },
+                    {
+                        name: '系统已经使用的内存',
+                        type: 'line',
+                        step:false,
+                        data: this.systemUseMemory,
+                        showSymbol: false
+                    }
+                ]
+            }
+        }
+    },
     created: function(){
         this.initMemoryData()
+        //连接
+        //this.connectWebsocket(),
+        //发送一个初始的查询条件
+        // this.sendDataMessage({
+        //     sessionId:"1234567890",
+        //     purpose:"0",
+        //     serverEnv:"dev",
+        //     serverKey:"test-Server",
+        //     instanceKey:"test-server-001",
+        //     startTime:1666829350124,
+        //     endTime:-1
+        // })
     },
     components:{
-        LineChart
+        EchartsPackage
+    },
+    watch: {
+        systemXAxisArray(){
+            //动态变更首尾数据
+            this.systemMemoryOption.xAxis.axisLabel.interval=this.systemXAxisArray.length-2
+        }
     }
 }
 </script>
